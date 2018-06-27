d1b6c1bfa345f54c8702523a02e30288ba82f264
Chad Chaney <cchane4@gmail.com>
2018-06-27 09:39:50 -0500

    fixing views and adding mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+
 
 var PORT = process.env.PORT || 3000;
 
@@ -8,7 +13,27 @@ var router = express.Router();
 
 app.use(express.static(__dirname + "/public"));
 
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+
+app.set('view engine', "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(router);
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// connecting to the database
+mongoose.connect(db, function(error){
+  if (error) {
+    console.log(error);
+  }
+  else{
+    console.log( "mongoose connection is successful");
+  }
+});
 
 app.listen(PORT, function(){
   console.log("listening on port:" + PORT);
