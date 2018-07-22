8f5ef760dbd17e2cfd3b907c7b74c48af287fd8f
Chad Chaney <cchane4@gmail.com>
2018-07-22 15:55:38 -0500

    adding styling and images

--- config/routes.js
+++ config/routes.js
@@ -7,4 +7,47 @@ module.exports = function(router){
   router.get("/saved", function(req, res){
     res.render("saved");
   });
+
+router.get("/api/fetch", function(req,res){
+    headlinesController.fetch(function(err,docs){
+        if(!docs || docs.insertedCount === 0){
+            res.json({
+                message: "No new articles. Check again later "
+            });
+        }
+        else {
+            res.json({
+                message: "Added" + docs.insteredCount + "new Articles!"
+            });
+        }
+
+
+    });
+  });
+
+  router.get("/api/headlines", function(req, res){
+    var query = {};
+    if (req.query.saved){
+        query = req.query;
+    }
+
+    headlinesController.get(query, function(data){
+        res.json(data);
+    });
+  });
+
+  router.delete("/api/headlines/:id", function(req, res){
+    var query = {};
+    query.id = req.params.id;
+    headlinesController.delete(query, function(err, data){
+        res.json(data);
+    });
+  });
+//runs and update function on what the user sends with their request
+  router.patch("/api/headlines", function(req, res){
+    headlinesController.update(req.body, function(err, data){
+        res.json(data);
+     });
+  });
+
 }
