1d9c588139331a60f5ee058b70d70eaaa0735292
Chad Chaney <cchane4@gmail.com>
2018-07-24 12:48:02 -0500

    adding scrape functionality

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){ 
+    var articleContainer = $("article-container"); 
+    $(document).on("click", ".btn-save", handleArticleSave); 
+    $(document).on("click", ".scrape-new", handleArticleScrape);
+    
+    initPage(); 
+
+    function initPage(){ 
+        articleContainer.empty(); 
+        $.get("/api/headline?save=false")
+        .then(function(data){ 
+            if (data && data.length){ 
+                rnederArticles(data); 
+            } 
+            else{ 
+                renderEmpty(); 
+            }
+
+            }); 
+}
+
+    function renderArticles(articles){ 
+    var articlePanels = []; 
+
+    for (var i = 0; i < artilces.length; i++){ 
+    articlePanels.push(createPanel(articles[i])); 
+}
+    articleContainer.append(articlePanels); 
+}
+
+
+function createPanel(article){ 
+  
+        var Panel = 
+        $([ "<div class='panel panel-default'>", 
+           "<div class='panel-heading'>", 
+            "<h3>",
+        article.headline, 
+        "<a> class='btn btn-sucess save'>", 
+        "Save Article",
+        "<a>", 
+        "<h3>", 
+        "<div>", 
+        "<div class='panel-body'>", 
+        article.summary, 
+        "</div>", 
+        "</div>"
+        ].join("")); 
+
+        panel.data("_id", article.id); 
+        return panel; 
+        
+}
+function renderEmpty(){ 
+    var emptyAlert = 
+    $(["<div class='alert alert-warning text-center'>", 
+        "<h4> Uh Oh. Looks like we don't have any new articles.</h4>", 
+        "<div>", 
+        "<div class='panel panel-default'>", 
+        "<div class='panel-heading text-center'>", 
+        "<h3>What Would You like to Do?</h3>", 
+        "<div>", 
+        "<div class='panel-body text-center'>", 
+        "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>", 
+        "</div>", 
+        "</div>"
+    ].join("")); 
+    articleContainer.append(emptyAlert); 
+} 
+
+function handleArticleSave(){
+    var articleToSave = $(this).parents('.panel').data();
+    articleToSave.saved = true; 
+
+    $.ajax({ 
+        method:"PATCH", 
+        url: "/api/headlines",
+        data: articleToSave
+    })
+    .then(function(data){ 
+        if (data.ok){ 
+            initPage(); 
+
+        }
+    });
+}
+function handleArticleScrape(){ 
+    $.get("/api/fetch")
+      .then(function(data){ 
+      
+        initPage();
+        bootbox.alert("<h3 class='text-center m-top-80'>" + data.message + "<h3>" ); 
+      }); 
+    } 
+
+});
